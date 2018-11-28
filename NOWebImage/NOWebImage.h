0a9865a3dc137734fa82eb42ad0eb9314fd3bcd9
董子江 <1329382531@qq.com>
2018-11-28 09:29:42 +0800

    Initial Commit

---
+++ NOWebImage/NOWebImage.h
@@ -0,0 +1,19 @@
+//
+//  NOWebImage.h
+//  NOWebImage
+//
+//  Created by 董子江 on 2018/11/28.
+//  Copyright © 2018 nightowl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NOWebImage.
+FOUNDATION_EXPORT double NOWebImageVersionNumber;
+
+//! Project version string for NOWebImage.
+FOUNDATION_EXPORT const unsigned char NOWebImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NOWebImage/PublicHeader.h>
+
+
